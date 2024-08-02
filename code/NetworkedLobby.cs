e2624a1156cd414b3365d83676000d509bab2455
PolSpock <5229571+PolSpock@users.noreply.github.com>
2024-08-03 00:14:23 +0200

    Initial commit

---
+++ code/NetworkedLobby.cs
@@ -0,0 +1,52 @@
+﻿using Sandbox.Network;
+
+namespace Sandbox
+{
+	public class NetworkedLobby : Component, Component.INetworkListener
+	{
+
+		protected override void OnAwake()
+		{
+			base.OnAwake();
+
+			if ( !GameNetworkSystem.IsActive )
+			{
+				GameNetworkSystem.CreateLobby();
+			}
+
+		}
+
+		public void OnActive( Connection channel )
+		{
+			Log.Info( $"Player '{channel.DisplayName}' is becoming active (" + channel.Id + ")" );
+
+			var goPlayer = new GameObject();
+			goPlayer.Name = "Player";
+			goPlayer.Parent = Game.ActiveScene;
+
+			var playerState = goPlayer.Components.Create<PlayerState>();
+
+			goPlayer.NetworkSpawn( channel );
+
+
+			RefreshPawn();
+
+		}
+
+		public void RefreshPawn()
+		{
+			Log.Info( "RefreshPawn" );
+			foreach ( var playerState in Game.ActiveScene.GetAllComponents<PlayerState>() )
+			{
+				if ( playerState.Pawn != null ) { continue; }
+
+				var go = new GameObject();
+				var pawn = go.Components.Create<Pawn>();
+
+				playerState.Pawn = pawn;
+
+				go.NetworkSpawn( playerState.Network.OwnerConnection );
+			}
+		}
+	}
+}
---
+++ code/PlayerState.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sandbox
+{
+	public class PlayerState : Component
+	{
+		[HostSync] public Pawn Pawn { get; set; }
+
+		protected override void OnUpdate()
+		{
+			base.OnUpdate();
+
+			Log.Info( "PlayerState OnUpdate" );
+			Log.Info( Network.OwnerConnection.DisplayName + " and his Pawn " + Pawn );
+		}
+	}
+}
